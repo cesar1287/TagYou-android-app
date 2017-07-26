03b5c422f4b500093d8a45edb140bfbc91b173af
César Rodrigues <rasec1287@hotmail.com>
2017-07-26 17:57:12 -0300

    Atualizado nav_header do menu principal

--- app/src/main/java/comcesar1287/github/tagyou/view/MainActivity.java
+++ app/src/main/java/comcesar1287/github/tagyou/view/MainActivity.java
@@ -1,7 +1,12 @@
 package comcesar1287.github.tagyou.view;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.support.design.widget.NavigationView;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,16 +17,25 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 
 import comcesar1287.github.tagyou.R;
+import comcesar1287.github.tagyou.controller.util.Utility;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    SharedPreferences sharedPreferences;
+
     private FirebaseAuth mAuth;
+
+    String name, email, profilePic, id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +56,27 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        sharedPreferences = getSharedPreferences(Utility.LOGIN_SHARED_PREF_NAME, MODE_PRIVATE);
+        id = sharedPreferences.getString("id", "0");
+        name = sharedPreferences.getString("name","Carregando...");
+        email = sharedPreferences.getString("email","Carregando...");
+        profilePic = sharedPreferences.getString("profile_pic","Carregando...");
+
+        View hView =  navigationView.getHeaderView(0);
+        final ImageView nav_image = (ImageView)hView.findViewById(R.id.imageView);
+        Glide.with(this).load(profilePic)
+                .asBitmap().into(new BitmapImageViewTarget(nav_image) {
+            @Override
+            protected void setResource(Bitmap resource) {
+                RoundedBitmapDrawable circularBitmapDrawable =
+                        RoundedBitmapDrawableFactory.create(getResources(), resource);
+                circularBitmapDrawable.setCircular(true);
+                nav_image.setImageDrawable(circularBitmapDrawable);
+            }
+        });
+        TextView nav_nome = (TextView)hView.findViewById(R.id.header_name);
+        nav_nome.setText(name);
     }
 
     public void signOut(View view){
