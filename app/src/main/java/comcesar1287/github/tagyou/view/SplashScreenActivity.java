7854473cc8788fe15326aa7fab4e6c1048260ed9
Islane <islane.junior@gmail.com>
2017-07-26 10:37:08 -0300

    Adição da splash

--- app/src/main/java/comcesar1287/github/tagyou/view/SplashScreenActivity.java
+++ app/src/main/java/comcesar1287/github/tagyou/view/SplashScreenActivity.java
@@ -2,8 +2,15 @@ package comcesar1287.github.tagyou.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.os.Handler;
 
 import comcesar1287.github.tagyou.R;
+
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -11,5 +18,23 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar!=null) {
+            actionBar.hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, SignInActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+
     }
+
 }
+
