b3ffea62d276bc7357782c6b0bf4a2c3c15ed915
Islane <islane.junior@gmail.com>
2017-08-04 12:02:35 -0300

    Adição da activity regirter e category_register

---
+++ app/src/main/java/comcesar1287/github/tagyou/view/CategoryRegisterActivity.java
@@ -0,0 +1,25 @@
+package comcesar1287.github.tagyou.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import comcesar1287.github.tagyou.R;
+
+public class CategoryRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_register);
+    }
+
+    public void registerPerson(View view) {
+
+
+    }
+
+
+    public void registerCompany(View view) {
+    }
+}
