ffd94b210131bfefd7c71fbd702b82072252d051
César Rodrigues <rasec1287@hotmail.com>
2017-08-04 14:38:19 -0300

    Terminada implementação registro de pessoa

--- app/src/main/java/comcesar1287/github/tagyou/view/RegisterPersonActivity.java
+++ app/src/main/java/comcesar1287/github/tagyou/view/RegisterPersonActivity.java
@@ -3,11 +3,14 @@ package comcesar1287.github.tagyou.view;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -27,15 +30,17 @@ import comcesar1287.github.tagyou.controller.domain.UserFacebook;
 import comcesar1287.github.tagyou.controller.firebase.FirebaseHelper;
 import comcesar1287.github.tagyou.controller.util.Utility;
 
-public class RegisterPersonActivity extends AppCompatActivity {
+public class RegisterPersonActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth;
 
     private DatabaseReference mDatabase;
 
-    private String Uid, name , email, profile_pic;
+    private String Uid, name , email, profile_pic, database, phone, birth;
 
     private SharedPreferences sp;
+
+    private TextInputLayout etName, etEmail, etPhone, etBirth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,19 +61,109 @@ public class RegisterPersonActivity extends AppCompatActivity {
             }
         });
 
-        EditText etName = (EditText) findViewById(R.id.register_edit_name);
-        etName.setText(userFacebook.getName());
-        EditText etEmail = (EditText) findViewById(R.id.register_edit_email);
-        etEmail.setText(userFacebook.getEmail());
+        etName = (TextInputLayout) findViewById(R.id.register_name);
+        etName.getEditText().setText(userFacebook.getName());
 
-        String database = getIntent().getStringExtra(Utility.KEY_CONTENT_EXTRA_DATABASE);
+        etEmail = (TextInputLayout) findViewById(R.id.register_email);
+        etEmail.getEditText().setText(userFacebook.getEmail());
+
+        etBirth = (TextInputLayout) findViewById(R.id.register_date);
+
+        etPhone = (TextInputLayout) findViewById(R.id.register_phone);
+
+        Button advance = (Button) findViewById(R.id.advance);
+        advance.setOnClickListener(this);
+
+        database = getIntent().getStringExtra(Utility.KEY_CONTENT_EXTRA_DATABASE);
 
         mAuth = FirebaseAuth.getInstance();
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        FirebaseUser user = mAuth.getCurrentUser();
-        finishLogin(user, database);
+        setupFieldMasks();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int id = view.getId();
+        switch (id){
+            case R.id.advance:
+                attemptLogin();
+                break;
+        }
+    }
+
+    public void setupFieldMasks(){
+        TextWatcher phoneMask = Utility.insertMask(getResources().getString(R.string.phone_mask), etPhone.getEditText());
+        etPhone.getEditText().addTextChangedListener(phoneMask);
+
+        TextWatcher birthMask = Utility.insertMask(getResources().getString(R.string.birth_mask), etBirth.getEditText());
+        etBirth.getEditText().addTextChangedListener(birthMask);
+    }
+
+    public void attemptLogin(){
+        String name, email, birth, phone;
+
+        boolean allFieldsFilled = true;
+        boolean allFilledRight = true;
+
+        name = etName.getEditText().getText().toString();
+        email = etEmail.getEditText().getText().toString();
+        birth = etBirth.getEditText().getText().toString();
+        phone = etPhone.getEditText().getText().toString();
+
+        if(name.equals("")){
+            allFieldsFilled = false;
+            etName.setError("Campo obrigatório");
+        }else{
+            etName.setErrorEnabled(false);
+        }
+
+        if(phone.equals("")){
+            allFieldsFilled = false;
+            etPhone.setError("Campo obrigatório");
+        }else{
+            etPhone.setErrorEnabled(false);
+        }
+
+        if(email.equals("")){
+            allFieldsFilled = false;
+            etEmail.setError("Campo obrigatório");
+        }else{
+            etEmail.setErrorEnabled(false);
+        }
+
+        if(birth.equals("")){
+            allFieldsFilled = false;
+            etBirth.setError("Campo obrigatório");
+        }else{
+            etBirth.setErrorEnabled(false);
+        }
+
+        if(allFieldsFilled) {
+
+            if (phone.length() < 14) {
+                allFilledRight = false;
+                etPhone.setError("Telefone inválido");
+            } else {
+                etPhone.setErrorEnabled(false);
+            }
+
+            if (birth.length() < 10) {
+                allFilledRight = false;
+                etBirth.setError("Data de nascimento inválida");
+            } else {
+                etBirth.setErrorEnabled(false);
+            }
+        }
+
+        if(allFieldsFilled && allFilledRight) {
+            FirebaseUser user = mAuth.getCurrentUser();
+            finishLogin(user, database);
+            Toast.makeText(this, "Cadastrado com sucesso", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
     public void finishLogin(FirebaseUser user, final String database){
@@ -77,6 +172,8 @@ public class RegisterPersonActivity extends AppCompatActivity {
         name = user.getDisplayName();
         email = user.getEmail();
         profile_pic = user.getPhotoUrl().toString();
+        phone = etPhone.getEditText().getText().toString();
+        birth = etBirth.getEditText().getText().toString();
 
         mDatabase.child(database).child(Uid).addListenerForSingleValueEvent(
                 new ValueEventListener() {
@@ -88,7 +185,7 @@ public class RegisterPersonActivity extends AppCompatActivity {
                         // [START_EXCLUDE]
                         if (user == null) {
 
-                            FirebaseHelper.writeNewUser(mDatabase, Uid, name, email, "", "", "", profile_pic);
+                            FirebaseHelper.writeNewUser(mDatabase, Uid, name, email, birth, "", phone, profile_pic);
 
                             sp = getSharedPreferences(Utility.LOGIN_SHARED_PREF_NAME, MODE_PRIVATE);
                             SharedPreferences.Editor editor = sp.edit();
