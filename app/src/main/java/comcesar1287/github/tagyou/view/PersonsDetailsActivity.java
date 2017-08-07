2fbd41816ccdfbba6ec5469beac34e300e22171b
Islane <islane.junior@gmail.com>
2017-08-07 16:52:49 -0300

    Adição das informações da tela de detalhes

---
+++ app/src/main/java/comcesar1287/github/tagyou/view/PersonsDetailsActivity.java
@@ -0,0 +1,47 @@
+package comcesar1287.github.tagyou.view;
+
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.support.design.widget.NavigationView;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+
+import comcesar1287.github.tagyou.R;
+import comcesar1287.github.tagyou.controller.domain.Person;
+import comcesar1287.github.tagyou.controller.util.Utility;
+
+public class PersonsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_persons_details);
+
+        Person person = (Person) getIntent().getSerializableExtra(Utility.KEY_CONTENT_EXTRA_COMPANY);
+
+        final ImageView nav_image = (ImageView) findViewById(R.id.image_details_person);
+        Glide.with(this).load(person.getProfilePic())
+                .asBitmap().into(new BitmapImageViewTarget(nav_image) {
+            @Override
+            protected void setResource(Bitmap resource) {
+                RoundedBitmapDrawable circularBitmapDrawable =
+                        RoundedBitmapDrawableFactory.create(getResources(), resource);
+                circularBitmapDrawable.setCircular(true);
+                nav_image.setImageDrawable(circularBitmapDrawable);
+            }
+        });
+        TextView nav_nome = (TextView) findViewById(R.id.name_details_person);
+        nav_nome.setText(person.getName());
+
+    }
+
+}
