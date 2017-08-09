0c1771a9d7488d002d6bd22fcdf2e25c43644fa9
César Rodrigues <rasec1287@hotmail.com>
2017-08-09 11:39:10 -0300

    Adicionado campo de hashtag no registro de pessoa

--- app/src/main/java/comcesar1287/github/tagyou/controller/domain/User.java
+++ app/src/main/java/comcesar1287/github/tagyou/controller/domain/User.java
@@ -5,18 +5,19 @@ import com.google.firebase.database.IgnoreExtraProperties;
 @IgnoreExtraProperties
 public class User {
 
-    public String name, email, birth, phone, sex, profile_pic;
+    public String name, email, birth, phone, sex, profile_pic, hashtag;
 
     public User() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String name, String email, String birth, String phone, String sex, String profile_pic) {
+    public User(String name, String email, String birth, String phone, String sex, String profile_pic, String hashtag) {
         this.name = name;
         this.email = email;
         this.birth = birth;
         this.phone = phone;
         this.sex = sex;
         this.profile_pic = profile_pic;
+        this.hashtag = hashtag;
     }
 }
--- app/src/main/java/comcesar1287/github/tagyou/controller/firebase/FirebaseHelper.java
+++ app/src/main/java/comcesar1287/github/tagyou/controller/firebase/FirebaseHelper.java
@@ -10,9 +10,9 @@ public class FirebaseHelper {
     public static final String FIREBASE_DATABASE_COMPANIES = "companies";
 
     public static void writeNewUser(DatabaseReference mDatabase, String userId, String name,
-                                    String email, String birth, String sex, String phone, String profile_pic) {
+                                    String email, String birth, String sex, String phone, String profile_pic, String hashtag) {
 
-        User user = new User(name, email, birth, phone, sex, profile_pic);
+        User user = new User(name, email, birth, phone, sex, profile_pic, hashtag);
 
         mDatabase.child(FIREBASE_DATABASE_USERS).child(userId).setValue(user);
     }
