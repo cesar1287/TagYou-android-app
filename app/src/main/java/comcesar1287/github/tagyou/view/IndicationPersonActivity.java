9e2146db7a217a8c9808044c7d4ef4f996ce436c
César Rodrigues <rasec1287@hotmail.com>
2017-08-18 15:13:02 -0300

    Terminada tela de indicação

--- app/src/main/java/comcesar1287/github/tagyou/view/IndicationPersonActivity.java
+++ app/src/main/java/comcesar1287/github/tagyou/view/IndicationPersonActivity.java
@@ -3,23 +3,22 @@ package comcesar1287.github.tagyou.view;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import comcesar1287.github.tagyou.R;
 
 public class IndicationPersonActivity extends AppCompatActivity {
+
+    TextView tvCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indication_person);
 
-        ActionBar actionBar = getSupportActionBar();
-
-        if(actionBar!=null){
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setElevation(0);
-        }
-
-
+        tvCode = (TextView) findViewById(R.id.code_indication);
+        tvCode.setText(FirebaseAuth.getInstance().getCurrentUser().getUid().substring(0,5));
     }
 }
