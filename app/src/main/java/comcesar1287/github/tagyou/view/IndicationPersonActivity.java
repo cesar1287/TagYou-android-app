54bdb144f6c5be9b452016b8d83743b19b7cbbd6
Islane <islane.junior@gmail.com>
2017-08-18 12:44:13 -0300

    Adição da tela de indicação de amigo e adição da opção no menu lateral

---
+++ app/src/main/java/comcesar1287/github/tagyou/view/IndicationPersonActivity.java
@@ -0,0 +1,15 @@
+package comcesar1287.github.tagyou.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comcesar1287.github.tagyou.R;
+
+public class IndicationPersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indication_person);
+    }
+}
