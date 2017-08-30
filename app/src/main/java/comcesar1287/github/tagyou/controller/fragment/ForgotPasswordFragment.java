2db3765c4945939f475a45ae4a298f606058807a
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2017-08-30 12:49:42 -0300

    Terminada implementação de recuperação de email

--- app/src/main/java/comcesar1287/github/tagyou/controller/fragment/ForgotPasswordFragment.java
+++ app/src/main/java/comcesar1287/github/tagyou/controller/fragment/ForgotPasswordFragment.java
@@ -4,13 +4,20 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import comcesar1287.github.tagyou.R;
 
 public class ForgotPasswordFragment extends DialogFragment {
 
+
+
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -24,12 +31,12 @@ public class ForgotPasswordFragment extends DialogFragment {
                 .setPositiveButton(R.string.forgot_password_button_send, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        // sign in the user ...
+                        //FirebaseAuth.getInstance().sendPasswordResetEmail()
                     }
                 })
                 .setNegativeButton(R.string.forgot_password_button_cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        //LoginDialogFragment.this.getDialog().cancel();
+                        Toast.makeText(getActivity(), "Recuperação de email cancelada", Toast.LENGTH_SHORT).show();
                     }
                 });
         return builder.create();
